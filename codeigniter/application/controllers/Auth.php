27e313263d9966e47db423e2b3598cf22f24831b
Akhilesh21 <akhilesh.mc4@gmail.com>
2020-02-05 16:33:03 +0530

    [Akhilesh] added try and catch for registerdata

--- codeigniter/application/controllers/Auth.php
+++ codeigniter/application/controllers/Auth.php
@@ -18,4 +18,24 @@ class Auth extends CI_Controller
         $db = 'Sample';
         $pwd = 'Admin@1234';
         $host = 'localhost';
+        try {
+            $dbObject = new PDO("mysql:host=$host;dbname=$db", $user, $pwd);
+            if (isset($_POST['email']) && isset($_POST['name']) && isset($_POST['number']) && isset($_POST['password'])) {
+                $email = $_POST['email'];
+                $password = $_POST['password'];
+                $name = $_POST['name'];
+                $number = $_POST['number'];
+                $query = "INSERT INTO registration (name,number,email,password )VALUES ('$name','$number','$email','$password')";
+                $stmt = $dbObject->prepare($query);
+                if ($stmt->execute()) {
+                    echo "Your data saved in database";
+                } else {
+                    echo "Plz enter proper details";
+                }
+                $dbObject = null;
+            }
+        } catch (PDOException $e) {
+            echo "connection failure";
+        }
+    }
    
