d045172090374e41f182de39bad18cfa9b14dc69
Akhilesh21 <akhilesh.mc4@gmail.com>
2020-02-05 17:13:50 +0530

    [Akhilesh] modified my test

--- codeigniter/application/controllers/Mytest.php
+++ codeigniter/application/controllers/Mytest.php
@@ -52,4 +52,60 @@ class Mytest
         return $data;
     }
 
-    
+    public function delete()
+    {
+        if (isset($_POST['ID'])) {
+            $form_data = array(
+                ':id' => $_POST['ID'],
+            );
+            $query = "DELETE FROM table1 WHERE ID=:id";
+            $statement = $this->connect->prepare($query);
+            if ($statement->execute($form_data)) {
+                $data[] = array(
+                    'success' => 'deleted',
+                );
+            } else {
+                $data[] = array(
+                    'fail' => 'not deleted',
+                );
+            }
+        } else {
+            $data[] = array(
+                'fail' => 'set the id',
+            );
+        }
+        echo json_encode($data);
+        return $data;
+    }
+
+    public function update()
+    {
+        if (isset($_POST["name"])) {
+            $form_data = array(
+                ':name' => $_POST['name'],
+                ':email' => $_POST['email'],
+                ':id' => $_POST['ID'],
+            );
+            $query = "UPDATE table1 SET name = :name, email = :email WHERE ID = :id";
+            $statement = $this->connect->prepare($query);
+            if ($statement->execute($form_data)) {
+                $data[] = array(
+                    'success' => 'inserted',
+                );
+            } else {
+                $data[] = array(
+                    'fail' => 'not inserted',
+                );
+            }
+        } else {
+            $data[] = array(
+                'fail' => 'set the name',
+            );
+        }
+        echo json_encode($data);
+        return $data;
+
+    }
+
+}
+?>
