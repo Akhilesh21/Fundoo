85e735c98e9b4ca307c13b9bc2f1952d6898cea4
Akhilesh21 <akhilesh.mc4@gmail.com>
2020-02-06 11:51:47 +0530

    [Akhilesh] added email readline validator programs

--- codeigniter/Filters/validateEmail.php
+++ codeigniter/Filters/validateEmail.php
@@ -1,10 +1,3 @@
 <?php
 echo "Enter email address: ";
 $email = readline();
-$vemail = filter_var($email, FILTER_VALIDATE_EMAIL);
-if($vemail == FALSE){
-    echo "Invalid Email \n";
-}else{
-    echo "Email is Valid ". $vemail ."\n";
-}
-?>
